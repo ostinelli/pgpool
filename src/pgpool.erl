7f63d2f99070258100fbf4051050978eb2d1eeef
Roberto Ostinelli <roberto@ostinelli.net>
2015-11-28 13:35:39 +0100

    Add proxy to worker function.

--- src/pgpool.erl
+++ src/pgpool.erl
@@ -2,7 +2,7 @@
 
 %% API
 -export([start/0, stop/0]).
-
+-export([squery/2, equery/3]).
 
 %% ===================================================================
 %% API
@@ -18,6 +18,14 @@ stop() ->
     ok = application:stop(pgpool),
     ok = application:stop(epgsql),
     ok = application:stop(poolboy).
+
+-spec squery(DatabaseName :: atom(), Sql :: string() | iodata()) -> any() | {error, no_connection}.
+squery(DatabaseName, Sql) ->
+    pgpool_worker:squery(DatabaseName, Sql).
+
+-spec equery(DatabaseName :: atom(), Statement :: string(), Params :: list()) -> any() | {error, no_connection}.
+equery(DatabaseName, Statement, Params) ->
+    pgpool_worker:equery(DatabaseName, Statement, Params).
 
 %% ===================================================================
 %% Internal
